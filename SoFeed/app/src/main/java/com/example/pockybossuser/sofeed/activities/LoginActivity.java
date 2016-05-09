060e9a30353b5c7de19129e338cb9ec4db377373
suppok <crystalpok@gmail.com>
2016-05-09 15:32:53 +0700

    Re init

--- SoFeed/app/src/main/java/com/example/pockybossuser/sofeed/activities/LoginActivity.java
+++ SoFeed/app/src/main/java/com/example/pockybossuser/sofeed/activities/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.example.pockybossuser.sofeed;
+package com.example.pockybossuser.sofeed.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.pockybossuser.sofeed.R;
 
 public class LoginActivity extends AppCompatActivity {
 
