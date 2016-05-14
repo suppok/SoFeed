ded4da83c6c93fdb0b7fa01c164bb58cf4e8a7f5
suppok <crystalpok@gmail.com>
2016-05-14 11:50:16 +0700

    NOW have facebook page

--- SoFeed/app/src/main/java/com/example/pockybossuser/sofeed/activities/LoginActivity.java
+++ SoFeed/app/src/main/java/com/example/pockybossuser/sofeed/activities/LoginActivity.java
@@ -1,15 +1,60 @@
 package com.example.pockybossuser.sofeed.activities;
 
+import android.support.v7.app.ActionBar;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.pockybossuser.sofeed.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView login_tv_username;
+    private TextView login_tv_password;
+    private EditText login_edtxt_username;
+    private EditText login_edtxt_password;
+    private Button login_btn_login;
+    private Button login_btn_register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        ActionBar bar = getSupportActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#00E676")));
+
+        login_tv_username = (TextView) findViewById(R.id.regis_tv_username);
+        login_tv_password = (TextView) findViewById(R.id.regis_tv_password);
+        login_edtxt_username = (EditText) findViewById(R.id.regis_etxt_username);
+        login_edtxt_password = (EditText) findViewById(R.id.regis_etxt_password);
+        login_btn_login = (Button) findViewById(R.id.login_btn_login);
+        login_btn_register = (Button) findViewById(R.id.login_btn_register);
+
+        login_btn_login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, FacebookActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login_btn_register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- SoFeed/app/src/main/java/com/example/pockybossuser/sofeed/activities/RegisterActivity.java
+++ SoFeed/app/src/main/java/com/example/pockybossuser/sofeed/activities/RegisterActivity.java
@@ -1,8 +1,61 @@
 package com.example.pockybossuser.sofeed.activities;
 
-/**
- * Created by pockybossuser on 5/9/2016.
- */
-public class RegisterActivity {
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.pockybossuser.sofeed.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextView regis_tv_username;
+    private EditText regis_etxt_username;
+    private TextView regis_tv_password;
+    private EditText regis_etxt_password;
+    private TextView regis_tv_confirmPass;
+    private EditText regis_etxt_confirmPass;
+    private TextView regis_tv_fbAccount;
+    private Button regis_btn_loginFB;
+    private Button regis_btn_save;
+    private Button regis_btn_cancel;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        ActionBar bar = getSupportActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#00E676")));
+
+        regis_etxt_username = (EditText) findViewById(R.id.regis_etxt_username);
+        regis_etxt_password = (EditText) findViewById(R.id.regis_etxt_password);
+        regis_etxt_confirmPass = (EditText) findViewById(R.id.regis_edt_confirmPass);
+        regis_btn_loginFB = (Button) findViewById(R.id.regis_btn_loginFB);
+        regis_btn_save = (Button) findViewById(R.id.regis_btn_save);
+        regis_btn_cancel = (Button) findViewById(R.id.regis_btn_cancel);
+
+        regis_btn_save.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        regis_btn_cancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
